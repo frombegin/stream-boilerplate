85ff437cb1644b83731fce5fb2685d62ed62cc1b
frombegin <frombegin@gmail.com>
2015-10-30 16:33:59 +0800

    Initial commit

---
+++ gdev/server/packet_handler.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+class PacketHandler(object):
+    def handle_packet(self, packet):
+        pass
---
+++ gdev/server/server.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+class Server(object):
+    pass
